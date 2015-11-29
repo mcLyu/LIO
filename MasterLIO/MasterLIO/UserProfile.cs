776a649cfa356ded227978b964e96f10c302953a
mcLyu <wolf1601@yandex.ru>
2015-11-29 14:12:12 +0400

    Добавил класс UserProfile

---
+++ MasterLIO/MasterLIO/UserProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterLIO
+{
+    class UserProfile
+    {
+        public String login {get;set;}
+        public String password { get; set; } 
+        public Role role { get; set; } 
+        //public Statistic statistic;
+    }
+
+    enum Role {ADMIN, STUDENT}
+}
