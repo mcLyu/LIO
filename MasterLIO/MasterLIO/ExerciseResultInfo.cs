a624714a4f21de989c8a98d2ffebca90c3e7ed62
mcLyu <wolf1601@yandex.ru>
2015-11-29 14:50:31 +0400

    Все необходимые классы-сущности

---
+++ MasterLIO/MasterLIO/ExerciseResultInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterLIO
+{
+    class ExerciseResultInfo
+    {
+        public Exercise exercise { get; set; }
+        public DateTime dateOfPassing { get; set; }//дата прохождения
+        public int errorsCount { get; set; }
+        public int assesment { get; set; }//оценка
+
+        public ExerciseResultInfo(Exercise exercise, DateTime dateOfPassing, int errorsCount, int assesment)
+        {
+            this.exercise=exercise;
+            this.dateOfPassing = dateOfPassing;
+            this.errorsCount=errorsCount;
+            this.assesment = assesment;
+        }
+    }
+}
