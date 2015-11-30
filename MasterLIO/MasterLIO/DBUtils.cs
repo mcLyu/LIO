a5c708837b6bab3bf0ac56b0084bd8de83d54b3a
mcLyu <wolf1601@yandex.ru>
2015-11-30 16:35:49 +0400

    Добавил класс DBUtils, реализует Настя

---
+++ MasterLIO/MasterLIO/DBUtils.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterLIO
+{
+    class DBUtils
+    {
+        public static UserProfile AuthorizeUser(String login, String password)
+        {
+            
+        }
+
+        public static UserProfile RegisterUser(String login, String password)
+        {
+
+        }
+
+        public static void RemoveUser(UserProfile user)
+        {
+
+        }
+
+        public static Statistic GetUserStatistic(UserProfile user)
+        {
+
+        }
+
+        public static UserProfile GetUserById(int userId)
+        {
+
+        }
+
+        public static void SaveExercise(Exercise exercise)
+        {
+
+        }
+
+        public static void LoadExercises()
+        {
+
+        }
+
+    }
+}
