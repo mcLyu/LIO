5453c44b9d796c47200d3c5f4ef54bf38b26916c
mcLyu <wolf1601@yandex.ru>
2016-01-18 15:10:10 +0400

    Create Form

---
+++ MasterLIO/MasterLIO/Forms/SelectAreaForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MasterLIO.Forms
+{
+    public partial class SelectAreaForm : Form
+    {
+        Graphics graph;
+        List<Point> points;
+        public SelectAreaForm()
+        {
+            graph = this.CreateGraphics();
+            InitializeComponent();
+            this.SetStyle(ControlStyles.SupportsTransparentBackColor, true); // в конструкторе
+            points = new List<Point>();
+        }
+
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            points.Add(new Point(e.X, e.Y));
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
